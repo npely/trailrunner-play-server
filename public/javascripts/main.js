259d578d43f0cf341f00ca8bc9e1600941f87a28
Peter Bemetz <peter.bemetz@t-online.de>
2020-11-24 17:58:13 +0100

    :tada: first js

---
+++ public/javascripts/main.js
@@ -0,0 +1,97 @@
+function initLevel(levelNumber) {
+}
+
+function moveUp() {
+}
+
+function moveDown() {
+}
+
+function moveLeft() {
+}
+
+function moveRight() {
+}
+
+function setFieldImage(fieldValue, isPlayerOnField) {
+    console.log("\nHallo, ich werde aufgerufen :)\n")
+    var myPicture;
+    const path = "images/fields/";
+    switch (fieldValue) {
+        case -99:
+            myPicture = path + "Wall.png";
+            break;
+        case -1:
+            if (isPlayerOnField) {
+                myPicture = path + "Ground_0.png";
+                break;
+            }
+        case 0:
+            if (isPlayerOnField) {
+                myPicture = path + "Ground_0_P.png";
+                break;
+            } else {
+                myPicture = path + "Ground_0.png";
+                break;
+            }
+        case 1:
+            if (isPlayerOnField) {
+                myPicture = path + "Ground_1_P.png";
+                break;
+            } else {
+                myPicture = path + "Ground_1.png";
+                break;
+            }
+        case 2:
+            if (isPlayerOnField) {
+                myPicture = path + "Ground_2_P.png";
+                break;
+            } else {
+                myPicture = path + "Ground_2.png";
+                break;
+            }
+        case 3:
+            if (isPlayerOnField) {
+                myPicture = path + "Ground_3_P.png";
+                break;
+            } else {
+                myPicture = path + "Ground_3.png";
+                break;
+            }
+        case 4:
+            if (isPlayerOnField) {
+                myPicture = path + "Ground_4_P.png";
+                break;
+            } else {
+                myPicture = path + "Ground_4.png";
+                break;
+            }
+        case 5:
+            if (isPlayerOnField) {
+                myPicture = path + "Ground_5_P.png";
+                break;
+            } else {
+                myPicture = path + "Ground_5.png";
+                break;
+            }
+        case -10:
+            myPicture = path + "Door1.png";
+            break;
+        // } else {
+        //     myPicture = path + "Door4.png";
+        //     break;
+        // }
+        case -20:
+            myPicture = path + "Door21.png";
+            break;
+        // } else {
+        //     myPicture = path + "Door24.png";
+        //     break;
+        // }
+        default:
+            myPicture = path + "Wall.png";
+    }
+
+    const image = document.getElementById("game-field");
+    image.src = myPicture;
+}
