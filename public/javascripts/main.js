58b61683ef9be6803cdbb82e91603f57f52f0e9f
Niklas Pelz <pelznik@gmail.com>
2020-11-25 23:18:03 +0100

    added the needed functions for the game to work
    Signed-off-by: Niklas Pelz <pelznik@gmail.com>

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,44 +1,194 @@
 function initLevel(levelNumber) {
-
+    window.location.href = `http://localhost:9000/level/${levelNumber}`;
+    buildLevel();
 }
 
-function moveUp(x) {
-    console.log("\nHallo, ich werde aufgerufen :)\n" + x.toString())
+function backToMain() {
+    console.log("Hallo")
+    window.location.href = "http://localhost:9000/menu";
+}
+
+function changeToLevelSelection() {
+    window.location.href = "http://localhost:9000/level";
+}
+
+function loadLevel() {
+    window.location.href = "http://localhost:9000/load";
+}
+
+function aboutGame() {
+    window.location.href = "http://localhost:9000/about";
+}
+
+function moveUp() {
+    let moveHttpRequest = new XMLHttpRequest();
+    let changeHttpRequest = new XMLHttpRequest();
+
+    changeHttpRequest.open('GET', '/changedFields/up', false);
+    changeHttpRequest.onreadystatechange = function () {
+        if(this.readyState === 4 && this.status === 200) {
+            const changedFields = JSON.parse(changeHttpRequest.responseText)
+            console.log(changedFields);
+            let newPlayerFieldCol = document.getElementById("col-" + changedFields.newPlayerY + "," + changedFields.newPlayerX);
+            if (changedFields.newPlayerFieldValue === -1) {
+                window.location.href = "http://localhost:9000/lose";
+            } else if (changedFields.newPlayerFieldValue === 9 || changedFields.newPlayerFieldValue === 19) {
+                window.location.href = "http://localhost:9000/win";
+            } else if (changedFields.newPlayerFieldValue === -100) {
+              return null;
+            } else {
+                updateField(newPlayerFieldCol, changedFields.newPlayerFieldValue, changedFields.newPlayerY, changedFields.newPlayerX, true);
+            }
+
+            let oldPlayerFieldCol = document.getElementById("col-" + changedFields.PlayerY + "," + changedFields.PlayerX);
+            updateField(oldPlayerFieldCol, changedFields.playerFieldValue, changedFields.playerY, changedFields.playerX, false);
+
+            let doorFieldCol = document.getElementById("col-" + changedFields.doorY + "," + changedFields.doorX);
+            if (changedFields.levelFieldSum === 0) {
+                updateField(doorFieldCol, changedFields.doorValue * -1, changedFields.doorY, changedFields.doorX, false);
+            } else {
+                updateField(doorFieldCol, changedFields.doorValue, changedFields.doorY, changedFields.doorX, false);
+            }
+        }
+        moveHttpRequest.open('GET', '/move/up', false);
+        moveHttpRequest.send()
+    }
+    changeHttpRequest.send()
 }
 
 function moveDown() {
+    let moveHttpRequest = new XMLHttpRequest();
+    let changeHttpRequest = new XMLHttpRequest();
+
+    changeHttpRequest.open('GET', '/changedFields/down', false);
+    changeHttpRequest.onreadystatechange = function () {
+        if(this.readyState === 4 && this.status === 200) {
+            const changedFields = JSON.parse(changeHttpRequest.responseText)
+            console.log(changedFields);
+            let newPlayerFieldCol = document.getElementById("col-" + changedFields.newPlayerY + "," + changedFields.newPlayerX);
+            if (changedFields.newPlayerFieldValue === -1) {
+                window.location.href = "http://localhost:9000/lose";
+            } else if (changedFields.newPlayerFieldValue === 9 || changedFields.newPlayerFieldValue === 19) {
+                window.location.href = "http://localhost:9000/win";
+            } else if (changedFields.newPlayerFieldValue === -100) {
+                return null;
+            } else {
+                updateField(newPlayerFieldCol, changedFields.newPlayerFieldValue, changedFields.newPlayerY, changedFields.newPlayerX, true);
+            }
+
+            let oldPlayerFieldCol = document.getElementById("col-" + changedFields.PlayerY + "," + changedFields.PlayerX);
+            updateField(oldPlayerFieldCol, changedFields.playerFieldValue, changedFields.playerY, changedFields.playerX, false);
+
+            let doorFieldCol = document.getElementById("col-" + changedFields.doorY + "," + changedFields.doorX);
+            if (changedFields.levelFieldSum === 0) {
+                updateField(doorFieldCol, changedFields.doorValue * -1, changedFields.doorY, changedFields.doorX, false);
+            } else {
+                updateField(doorFieldCol, changedFields.doorValue, changedFields.doorY, changedFields.doorX, false);
+            }
+        }
+        moveHttpRequest.open('GET', '/move/down', false);
+        moveHttpRequest.send()
+    }
+    changeHttpRequest.send()
 }
 
 function moveLeft() {
+    let moveHttpRequest = new XMLHttpRequest();
+    let changeHttpRequest = new XMLHttpRequest();
+
+    changeHttpRequest.open('GET', '/changedFields/left', false);
+    changeHttpRequest.onreadystatechange = function () {
+        if(this.readyState === 4 && this.status === 200) {
+            const changedFields = JSON.parse(changeHttpRequest.responseText)
+            console.log(changedFields);
+            let newPlayerFieldCol = document.getElementById("col-" + changedFields.newPlayerY + "," + changedFields.newPlayerX);
+            if (changedFields.newPlayerFieldValue === -1) {
+                window.location.href = "http://localhost:9000/lose";
+            } else if (changedFields.newPlayerFieldValue === 9 || changedFields.newPlayerFieldValue === 19) {
+                window.location.href = "http://localhost:9000/win";
+            } else if (changedFields.newPlayerFieldValue === -100) {
+                return null;
+            } else {
+                updateField(newPlayerFieldCol, changedFields.newPlayerFieldValue, changedFields.newPlayerY, changedFields.newPlayerX, true);
+            }
+
+            let oldPlayerFieldCol = document.getElementById("col-" + changedFields.PlayerY + "," + changedFields.PlayerX);
+            updateField(oldPlayerFieldCol, changedFields.playerFieldValue, changedFields.playerY, changedFields.playerX, false);
+
+            let doorFieldCol = document.getElementById("col-" + changedFields.doorY + "," + changedFields.doorX);
+            if (changedFields.levelFieldSum === 0) {
+                updateField(doorFieldCol, changedFields.doorValue * -1, changedFields.doorY, changedFields.doorX, false);
+            } else {
+                updateField(doorFieldCol, changedFields.doorValue, changedFields.doorY, changedFields.doorX, false);
+            }
+        }
+        moveHttpRequest.open('GET', '/move/left', false);
+        moveHttpRequest.send()
+    }
+    changeHttpRequest.send()
 }
 
 function moveRight() {
+    let moveHttpRequest = new XMLHttpRequest();
+    let changeHttpRequest = new XMLHttpRequest();
+
+    changeHttpRequest.open('GET', '/changedFields/right', false);
+    changeHttpRequest.onreadystatechange = function () {
+        if(this.readyState === 4 && this.status === 200) {
+            const changedFields = JSON.parse(changeHttpRequest.responseText)
+            console.log(changedFields);
+            let newPlayerFieldCol = document.getElementById("col-" + changedFields.newPlayerY + "," + changedFields.newPlayerX);
+            if (changedFields.newPlayerFieldValue === -1) {
+                window.location.href = "http://localhost:9000/lose";
+            } else if (changedFields.newPlayerFieldValue === 9 || changedFields.newPlayerFieldValue === 19) {
+                window.location.href = "http://localhost:9000/win";
+            } else if (changedFields.newPlayerFieldValue === -100) {
+                return null;
+            } else {
+                updateField(newPlayerFieldCol, changedFields.newPlayerFieldValue, changedFields.newPlayerY, changedFields.newPlayerX, true);
+            }
+
+            let oldPlayerFieldCol = document.getElementById("col-" + changedFields.PlayerY + "," + changedFields.PlayerX);
+            updateField(oldPlayerFieldCol, changedFields.playerFieldValue, changedFields.playerY, changedFields.playerX, false);
+
+            let doorFieldCol = document.getElementById("col-" + changedFields.doorY + "," + changedFields.doorX);
+            if (changedFields.levelFieldSum === 0) {
+                updateField(doorFieldCol, changedFields.doorValue * -1, changedFields.doorY, changedFields.doorX, false);
+            } else {
+                updateField(doorFieldCol, changedFields.doorValue, changedFields.doorY, changedFields.doorX, false);
+            }
+        }
+        moveHttpRequest.open('GET', '/move/right', false);
+        moveHttpRequest.send()
+    }
+    changeHttpRequest.send()
 }
 
 function buildLevel() {
-    var httpRequest = new XMLHttpRequest();
+    let httpRequest = new XMLHttpRequest();
     httpRequest.open('GET', "/levelMap", true);
     httpRequest.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState === 4 && this.status === 200) {
             const mapData = JSON.parse(httpRequest.responseText)
-            var parent = document.getElementById("level-map");
-            for (x = 0; x < 10; x++) {
-                var row = document.createElement('div');
+            let parent = document.getElementById("level-map");
+            for (let x = 0; x < 10; x++) {
+                let row = document.createElement('div');
                 row.classList.add('row');
                 row.classList.add('justify-content-center');
                 row.id = "row-" + x.toString();
                 parent.append(row);
-                for (y = 0; y < 10; y++) {
-                    var col = document.createElement('div');
+                for (let y = 0; y < 10; y++) {
+                    let col = document.createElement('div');
                     col.classList.add('col');
-                    col.id = "no-padding";
+                    col.classList.add('no-padding');
+                    col.id = "col-" + x + "," + y;
                     row.append(col);
-                    if (mapData.level.xPos == y && mapData.level.yPos == x) {
+                    if (mapData.level.xPos === y && mapData.level.yPos === x) {
                         //create image
-                        setFieldImage(col, mapData.fieldvalues[x * 10 + y].fieldvalue, true);
+                        buildField(col, mapData.fieldvalues[x * 10 + y].fieldvalue, x, y, true);
                     } else {
                         //create image
-                        setFieldImage(col, mapData.fieldvalues[x * 10 + y].fieldvalue, false);
+                        buildField(col, mapData.fieldvalues[x * 10 + y].fieldvalue, x, y, false);
                     }
                 }
             }
@@ -47,19 +197,23 @@ function buildLevel() {
     httpRequest.send();
 }
 
-function buildField(parent, source) {
-    var image = document.createElement('img');
+function updateField(parent, fieldValue, x, y, isPlayerOnField) {
+    let image = document.getElementById("img-" + x + y);
+    image.src = 'http://localhost:9000/assets/' + setFieldImage(parent, fieldValue, isPlayerOnField);
+}
+
+function buildField(parent, fieldValue, x, y, isPlayerOnField) {
+    let image = document.createElement('img');
     //temporal work-around
-    image.src = 'http://localhost:9000/assets/' + source;
-    image.id = "game-field";
+    image.src = 'http://localhost:9000/assets/' + setFieldImage(parent, fieldValue, isPlayerOnField);
+    image.classList.add("game-field");
+    image.id = "img-" + x + y;
     parent.append(image);
 }
 
-
 function setFieldImage(parent, fieldValue, isPlayerOnField) {
-    console.log("\nHallo, ich werde aufgerufen :)\n")
     const path = "images/fields/";
-    var myPicture = path;
+    let myPicture = path;
     switch (fieldValue) {
         case -99:
             myPicture += "Wall.png";
@@ -117,22 +271,20 @@ function setFieldImage(parent, fieldValue, isPlayerOnField) {
                 myPicture += "Ground_5.png";
                 break;
             }
+        case 10:
+            myPicture += "Door4.png";
+            break;
+        case 20:
+            myPicture += "Door24.png";
+            break;
         case -10:
             myPicture += "Door1.png";
             break;
-        // } else {
-        //     myPicture = path + "Door4.png";
-        //     break;
-        // }
         case -20:
             myPicture += "Door21.png";
             break;
-        // } else {
-        //     myPicture = path + "Door24.png";
-        //     break;
-        // }
         default:
             myPicture += "Wall.png";
     }
-    buildField(parent, myPicture)
+    return myPicture;
 }
